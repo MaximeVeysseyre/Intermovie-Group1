d58655e3974a542fb454726d2bd24515226d4442
Thibault <thibault.goutorbe19@gmail.com>
2020-09-25 13:15:21 +0200

    ajout version "clean" des requêetes

---
+++ src/credentials.py
@@ -0,0 +1,10 @@
+import os
+
+class Credentials():
+    RAW_LOCAL_PATH = os.getcwd() + os.sep + 'data' + os.sep + 'RAW' + os.sep
+    CURATED_LOCAL_PATH = os.getcwd() + os.sep + 'data' + os.sep + 'CURATED' + os.sep
+    TITLE_BASICS = r'title.basics.tsv'
+    TITLE_AKAS = r'title.akas.tsv'
+    TITLE_PRINCIPALS = r'title.principals.tsv'
+    TITLE_RATINGS = r'titles.ratings.tsv'
+    NAME_BASICS = r'name.basics.tsv'
